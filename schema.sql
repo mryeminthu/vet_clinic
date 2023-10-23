3d76e05b17bc5639973e2a32a2412f23bcabffde
mryeminthu <dhammaarchive@hotmail.com>
2023-10-23 12:28:04 -0700

    Add to schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(200),
+    date_of_birth DATE,
+    escape_attempts INTEGER,
+    neutered BOOLEAN,
+    weight_kg  NUMERIC(5,2)
 );
