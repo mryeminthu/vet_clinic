16a4ca519193c5befedd1ee93dddd6925ddf95ec
mryeminthu <dhammaarchive@hotmail.com>
2023-10-23 12:34:00 -0700

    Add data

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES
+ ('Agumon', '2020-2-3', 10.23, TRUE, 0),
+ ('Gabumon', '2018-11-15', 8, TRUE, 2),
+ ('Pikachu', '2021-1-7', 15.04, FALSE, 1),
+ ('Devimon', '2017-5-12', 11, TRUE, 5);
